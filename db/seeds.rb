41c922d2db7bdc5940659ea4a2b38fbc314b615e
Amanda Sulistyo amandaz.sulistyo@gmail.com <amandaz.sulistyo@gmail.com>
2025-01-20 22:15:24 +0800

    added review model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "The Wandering Stairs", address: "The Crystarium (X10 Y15)")
+puts "Created Dishoom"
+Restaurant.create!(name: "The Dwarven Dive", address: "Kholusia (X18 Y18)")
+puts "Created Pizza East"
+Restaurant.create!(name: "The Last Stand", address: "Old Sharlayan")
+puts "The Last Stand"
+Restaurant.create!(name: "The Teahouse", address: "Kugane (X10 Y10)")
+puts "The Teahouse"
+Restaurant.create!(name: "The Alehouse", address: "South Shroud (X18 Y20)")
+puts "The Alehouse"
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
